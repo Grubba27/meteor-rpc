397110f18c2ddfeef9bd72bf4adfd3c2d9690e23
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-12-19 15:19:23 -0300

    feat 0.9.3

--- lib/createClient.ts
+++ lib/createClient.ts
@@ -2,7 +2,7 @@ import { createMethod } from "./createMethod";
 import { Meteor } from "meteor/meteor";
 import { z } from "zod";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import {  useMutation as useMutationRQ } from "@tanstack/react-query";
+import { useMutation as useMutationRQ } from "@tanstack/react-query";
 
 type M = ReturnType<typeof createMethod>;
 type R = Record<string, M>;
@@ -32,54 +32,54 @@ export const createClient = <T>() => createProxyClient<T>() as T;
 const createProxyClient = <T extends R, Prop = keyof T>(
   path: string[] = []
 ) => {
-  const proxy = new Proxy(
-    {},
-    {
-      get(_, key: string) {
-        if (typeof key !== "string" || key === "then" || key === "toJSON") {
-          // special case for if the proxy is accidentally treated
-          // like a PromiseLike (like in `Promise.resolve(proxy)`)
-          return undefined;
-        }
-        return createProxyClient([...path, key]);
-      },
-      apply(_1, _2, args) {
-        const lastArg = args[args.length - 1];
-        if (lastArg === "useQuery" || lastArg === "useMutation") {
-          args.pop();
-        }
-        const name = path.join(".");
+  const proxy = new Proxy(() => {}, {
+    get(_, key: string) {
+      if (typeof key !== "string" || key === "then" || key === "toJSON") {
+        // special case for if the proxy is accidentally treated
+        // like a PromiseLike (like in `Promise.resolve(proxy)`)
+        return undefined;
+      }
+      return createProxyClient([...path, key]);
+    },
+    apply(_1, _2, args) {
+      const lastArg = args.at(-1);
+      if (lastArg === "useQuery" || lastArg === "useMutation") {
+        args.pop();
+      }
+      const name = path.join(".");
 
-        function call(...params) {
-          return new Promise((resolve, reject) => {
-            Meteor.call(name, ...params, (err: null | Meteor.Error, result: any) => {
+      function call(...params) {
+        return new Promise((resolve, reject) => {
+          Meteor.call(
+            name,
+            ...params,
+            (err: null | Meteor.Error, result: any) => {
               if (err) {
                 reject(err);
               } else {
                 resolve(result);
               }
-            });
-          });
-        }
+            }
+          );
+        });
+      }
 
-        if (lastArg === "useQuery") {
-          return useSuspenseQuery({
-            queryKey: [name, ...args],
-            queryFn: () => call(...args)
-          });
-        }
+      if (lastArg === "useQuery") {
+        return useSuspenseQuery({
+          queryKey: [name, ...args],
+          queryFn: () => call(...args),
+        });
+      }
 
-        if (lastArg === "useMutation") {
-          return useMutationRQ({
-            mutationFn: (params) => call(params),
-          });
-        }
+      if (lastArg === "useMutation") {
+        return useMutationRQ({
+          mutationFn: (params) => call(params),
+        });
+      }
 
-        return call(...args);
-      },
-    }
-  ) as T;
+      return call(...args);
+    },
+  }) as unknown as T;
 
   return proxy;
 };
-
